28f604046653502dc2cac7d4b7326e525d8d06c3
WesleySniperFUT <64104580+WesleySniperFUT@users.noreply.github.com>
2020-10-12 12:02:20 +0200

    Update autobuyer.user.js

--- autobuyer.user.js
+++ autobuyer.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         FUT20 Autobuyer
+// @name         FUT21 Autobuyer
 // @namespace    http://tampermonkey.net/
 // @version      0.6
 // @description  try to take over the world!
